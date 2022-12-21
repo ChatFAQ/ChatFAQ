d07a46fe274fc5dbb8155a4c9f241b9a336b1795
Hector Soria <attosoria@hotmail.com>
2022-12-21 15:07:54 +0100

    some docstrings

--- back/riddler/apps/broker/models/message.py
+++ back/riddler/apps/broker/models/message.py
@@ -28,27 +28,52 @@ class StackPayloadType(Enum):
 
 class Message(ChangesMixin):
     """
-    ivar str prev: The id of the previous MML, typically to which this one answers. Thanks to this we can reconstruct the whole conversation in order.
-    ivar JSONField transmitter: The type of agent (human/bot) that generated this message.
-        str first_name: The first name of the sending agent
-        str last_name: The last name of the sending agent
-        str type: Its type: bot or human
-    ivar JSONField receiver: The agent to which this message is intended. Is this property ****required? Could it be the transmitter is entirely unknown to whom is communicating?
-    ivar str conversation: A unique identifier that groups all the messages sent within the same context.
-    ivar str send_time: The moment at which this message was sent.
-    ivar float confidence: How certain the bot is about its answer (required when transmitter = bot)
-    ivar float threshold: The minimal confidence the user would accept from the bot (required when transmitter = human)
-    ivar JSONField meta: any extra info out of the bot domain the agen considers to put in
-    ivar JSONField payload: contains the payload of the answer or question of the agent, it needs to have at least one item but it could contain an indefinite number of them.
-        str text: Plain text
-        str html: HTML that should be interpreted
-        str image: Image content
-        JSONField quick_replies: A list of CTAs. If this field is present it needs to contain at least 1 item. It is assumed that if it only contains an **id** it would mean is a response (a choose quick_replied) of a previous MML, being this id the same as the choose CTA
-            str text: The text to display on the CTA button
-            str id: an identifier to use later on as a reference in the response
-            JSONField meta: any extra info out of the bot domain the agen considers to put in
-        str response_id: In case the response is an indexed item on a database (as such the answer of a FAQ)
-        str satisfaction: For the user to express its satisfaction to the given bot’s answer
+    The representation of the MML as a Django model
+
+    Attributes
+    ----------
+    prev: str
+        The id of the previous MML, typically to which this one answers. Thanks to this we can reconstruct the whole conversation in order.
+    transmitter: JSONField
+        The type of agent (human/bot) that generated this message.
+        * first_name str:
+            The first name of the sending agent
+        * last_name str:
+            The last name of the sending agent
+        * type str:
+            Its type: bot or human
+    receiver: JSONField
+        The agent to which this message is intended. Is this property ****required? Could it be the transmitter is entirely unknown to whom is communicating?
+    conversation: str
+        A unique identifier that groups all the messages sent within the same context.
+    send_time: str
+        The moment at which this message was sent.
+    confidence: float
+        How certain the bot is about its answer (required when transmitter = bot)
+    threshold: float
+        The minimal confidence the user would accept from the bot (required when transmitter = human)
+    meta: JSONField
+        any extra info out of the bot domain the agen considers to put in
+    stacks: list
+        contains the payload of the message itself.
+        * text str:
+            Plain text
+        * html str:
+            HTML that should be interpreted
+        * image str:
+            Image content
+        * quick_replies JSONField:
+            A list of CTAs. If this field is present it needs to contain at least 1 item. It is assumed that if it only contains an **id** it would mean is a response (a choose quick_replied) of a previous MML, being this id the same as the choose CTA
+            * text str:
+                The text to display on the CTA button
+            * id str:
+                an identifier to use later on as a reference in the response
+            * meta JSONField:
+                any extra info out of the bot domain the agen considers to put in
+        * response_id str:
+            In case the response is an indexed item on a database (as such the answer of a FAQ)
+        * satisfaction str:
+            For the user to express its satisfaction to the given bot’s answer
     """
 
     prev = models.ForeignKey("self", null=True, unique=True, on_delete=models.SET_NULL)
--- back/riddler/apps/broker/models/platform_bot.py
+++ back/riddler/apps/broker/models/platform_bot.py
@@ -14,12 +14,24 @@ class PlatformTypes(Enum):
 class PlatformBot(ChangesMixin):
     """
     This represents the association between a message platform and a FSM
+    Attributes
+    ----------
+    fsm
+        A reference to the finite state machine model
+    platform_type : str
+        Telegram, Whatsapp, etc...
+    platform_meta : dict
+        metadata specific to the platform itself, they often are tokens, api_urls, etc...
     """
     fsm = models.ForeignKey(FiniteStateMachine, on_delete=models.CASCADE)
     platform_type = models.CharField(max_length=255, choices=((v.value, v.value) for v in PlatformTypes))
     platform_meta = models.JSONField(default=dict)
 
     def platform_view_name(self):
+        """
+        Just an utility function to control the url's view name depending on the platform type since this name will
+        most likely depend on the metadata of the paltform
+        """
         if self.platform_type == PlatformTypes.telegram.value:
             return f"webhook_{self.platform_type}_{self.platform_meta['token'].replace(':', '_')}"
         else:
