3a7907165639bd732ae1a11a8f2f6bfcb8caf1f4
Hector Soria <attosoria@hotmail.com>
2023-01-11 15:44:05 +0100

    pretty json widget on admin

--- back/riddler/apps/fsm/admin.py
+++ back/riddler/apps/fsm/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.contrib.postgres.fields import ArrayField as DjangoArrayField
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
-from django_better_admin_arrayfield.forms.widgets import DynamicArrayTextareaWidget
-
+from django.db.models import JSONField
 from .models import CachedFSM, FSMDefinition
+from ...utils import PrettyJSONWidget
 
 
 class FSMDefinitionAdmin(admin.ModelAdmin, DynamicArrayMixin):
     formfield_overrides = {
-        DjangoArrayField: {"widget": DynamicArrayTextareaWidget},
+        JSONField: {'widget': PrettyJSONWidget}
     }
 
 
