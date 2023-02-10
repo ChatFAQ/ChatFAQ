abd09c72d79b2ca47d125410953ddfd947b9a70c
Kevin Maikath <kevin.maikath@with-madrid.com>
2023-02-10 13:18:37 +0100

    widget: fixed reactivity issue with fetch

--- widget/components/FSMSelector.vue
+++ widget/components/FSMSelector.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="selector-wrapper">
         <div>Select Your FSM</div>
-        <SelectButton v-model="selectedFSMDef" :options="fsmDefs" optionLabel="name"/>
+        <SelectButton v-model="selectedFSMDef" :options="fsmDefs" optionLabel="name" />
     </div>
 </template>
 
 <script setup>
-    const runtimeConfig = useRuntimeConfig()
-    const { data: fsmDefs } = await useFetch(runtimeConfig.riddlerAPI + "/back/api/fsm/definitions/?fields=id,name")
-    const selectedFSMDef = useState("selectedFSMDef")
+const fsmDefs = ref(null);
+const runtimeConfig = useRuntimeConfig();
+const selectedFSMDef = useState("selectedFSMDef");
+
+const res = await fetch(runtimeConfig.riddlerAPI + "/back/api/fsm/definitions/?fields=id,name");
+fsmDefs.value = await res.json();
 </script>
 
 <style scoped lang="scss">
-    .selector-wrapper{
-        text-align: center;
-        * {
-            margin: 20px;
-        }
+.selector-wrapper {
+    text-align: center;
+
+    * {
+        margin: 20px;
     }
+}
 </style>
