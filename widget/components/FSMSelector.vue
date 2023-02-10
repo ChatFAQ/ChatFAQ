a3e411c162c91923e10a390b06f7b457a270bbc6
Hector Soria <attosoria@hotmail.com>
2023-02-10 13:49:53 +0100

    Going back to useFetch

--- widget/components/FSMSelector.vue
+++ widget/components/FSMSelector.vue
@@ -1,25 +1,21 @@
 <template>
     <div class="selector-wrapper">
         <div>Select Your FSM</div>
-        <SelectButton v-model="selectedFSMDef" :options="fsmDefs" optionLabel="name" />
+        <SelectButton v-model="selectedFSMDef" :options="fsmDefs" optionLabel="name"/>
     </div>
 </template>
 
 <script setup>
-const fsmDefs = ref(null);
-const runtimeConfig = useRuntimeConfig();
-const selectedFSMDef = useState("selectedFSMDef");
-
-const res = await fetch(runtimeConfig.riddlerAPI + "/back/api/fsm/definitions/?fields=id,name");
-fsmDefs.value = await res.json();
+    const runtimeConfig = useRuntimeConfig()
+    const { data: fsmDefs } = await useFetch(runtimeConfig.riddlerAPI + "/back/api/fsm/definitions/?fields=id,name")
+    const selectedFSMDef = useState("selectedFSMDef")
 </script>
 
 <style scoped lang="scss">
-.selector-wrapper {
-    text-align: center;
-
-    * {
-        margin: 20px;
+    .selector-wrapper{
+        text-align: center;
+        * {
+            margin: 20px;
+        }
     }
-}
 </style>
