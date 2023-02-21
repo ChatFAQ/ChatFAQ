bb497eecff74bb2de33d5bfc1dd889358acd773d
Hector Soria <attosoria@hotmail.com>
2023-02-21 15:41:28 +0100

    format

--- widget/components/_index.js
+++ widget/components/_index.js
@@ -8,29 +8,21 @@ import ToastService from 'primevue/toastservice'
 import SelectButton from "primevue/selectbutton";
 import TextArea from "primevue/textarea";
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+
 function loadWidget(selector) {
     const el = document.querySelector(selector);
-    createApp(
-        WidgetLoader, { ...el.dataset }
-    ).use(
-        createPinia()
-    ).use(
-        PrimeVue, { ripple: true }
-    ).use(
-        ToastService
-    ).component(
-        'Button', Button
-    ).component(
-        'InputText', InputText
-    ).component(
-        'Toast', Toast
-    ).component(
-        'SelectButton', SelectButton
-    ).component(
-        'TextArea', TextArea
-    ).use(
-        createPinia()
-    ).mount(selector);
+    createApp(WidgetLoader, {...el.dataset})
+        .use(createPinia())
+        .use(PrimeVue, {ripple: true})
+        .use(ToastService)
+        .use(createPinia())
+        .component('Button', Button)
+        .component('InputText', InputText)
+        .component('Toast', Toast)
+        .component('SelectButton', SelectButton)
+        .component('TextArea', TextArea)
+        .mount(selector);
 }
-export { loadWidget }
+
+export {loadWidget}
