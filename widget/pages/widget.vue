aa9366d8a7dccaa85e61a409c4e7f39d3fa670dd
Hector Soria <attosoria@hotmail.com>
2023-02-21 16:27:46 +0100

    fixing nuxt version

--- widget/pages/widget.vue
+++ widget/pages/widget.vue
@@ -3,3 +3,12 @@
     <Widget />
   </div>
 </template>
+<script setup>
+import { useGlobalStore } from "~/store";
+const store = useGlobalStore()
+const runtimeConfig = useRuntimeConfig()
+
+store.chatfaqWS = runtimeConfig.chatfaqWS;
+store.chatfaqAPI = runtimeConfig.chatfaqAPI;
+
+</script>
