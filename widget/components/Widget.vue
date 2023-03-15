7604f33ed7f4bab8b75268b967d1d8656870c118
Hector Soria <attosoria@hotmail.com>
2023-03-15 12:44:45 +0100

    history panel

--- widget/components/Widget.vue
+++ widget/components/Widget.vue
@@ -5,9 +5,9 @@
                 <div v-if="store.historyOpened" class="widget-history" :class="{'maximized': store.maximized}">
                 </div>
                 <div class="flex-column" :class="{'maximized': store.maximized}">
-                    <Header class="header"/>
-                    <Chat class="chat"/>
-                    <Footer class="footer"/>
+                    <Header class="header" :class="{'history': store.historyOpened}"/>
+                    <Chat class="chat" :class="{'history': store.historyOpened}"/>
+                    <Footer class="footer" :class="{'history': store.historyOpened}"/>
                 </div>
             </div>
             <div class="widget-open-button" @click="opened = !opened"><i :class="opened ? 'close' : 'open'"/></div>
@@ -42,7 +42,16 @@ store.subtitle = props.subtitle;
 $widget-open-button-margin: 24px;
 
 .widget-history {
-    background-color: red;
+    background: $chatfaq-color-gradient-purple;
+    width: 220px;
+    height: 580px;
+    &.maximized {
+        height: 85vh;
+    }
+    border-radius: 10px 0px 0px 10px;
+    border-top: 2px solid $chatfaq-color-primary-500;
+    border-bottom: 2px solid $chatfaq-color-primary-500;
+    border-left: 2px solid $chatfaq-color-primary-500;
 }
 .widget-wrapper {
     position: absolute;
@@ -70,16 +79,27 @@ $widget-open-button-margin: 24px;
 .widget-wrapper > .flex-column > .header {
     border: 2px solid $chatfaq-color-primary-500;
     border-radius: 10px 10px 0px 0px;
+    &.history {
+        border-radius: 0px 10px 0px 0px;
+        border-left: 0px;
+    }
 }
 .widget-wrapper > .flex-column > .chat {
     position: relative;
     height: 100%;
     border-left: 2px solid $chatfaq-color-primary-500;
     border-right: 2px solid $chatfaq-color-primary-500;
+    &.history {
+        border-left: 0px;
+    }
 }
 .widget-wrapper > .flex-column > .footer {
     border: 2px solid $chatfaq-color-primary-500;
     border-radius: 0px 0px 10px 10px;
+    &.history {
+        border-radius: 0px 0px 10px 0px;
+        border-left: 0px;
+    }
 }
 
 
