243733b83ee9323685972f1549237c8b8abd3a94
Hector Soria <hector.soria@with-madrid.com>
2023-03-28 12:58:39 +0200

    Some logging for deployment debugging

--- widget/pages/index.vue
+++ widget/pages/index.vue
@@ -16,7 +16,9 @@ if(!userId) {
 
 const runtimeConfig = useRuntimeConfig()
 console.log("PUBLIC CONFIG:::")
-console.log(runtimeConfig.public)
+console.log(runtimeConfig)
+console.log(runtimeConfig.public.chatfaqWS)
+console.log(runtimeConfig.public.chatfaqAPI)
 console.log(":::")
 const title = ref("Hello there 👋")
 const subtitle = ref("How can we help you?")
