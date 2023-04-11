b19c253d61985854d6558c5f00311e684847f94f
Hector Soria <hector.soria@with-madrid.com>
2023-04-11 16:04:39 +0200

    making the widget example work in ssr mode

--- widget/pages/index.vue
+++ widget/pages/index.vue
@@ -1,26 +1,32 @@
 <template>
-    <Widget :chatfaqWs="chatfaqWS" :chatfaqApi="chatfaqAPI" :fsmDef="fsmDef" :userId="userId" :title="title" :subtitle="subtitle"/>
+    <client-only>
+        <Widget :chatfaqWs="chatfaqWS" :chatfaqApi="chatfaqAPI" :fsmDef="fsmDef" :userId="userId" :title="title"
+                :subtitle="subtitle"/>
+    </client-only>
 </template>
 <script setup>
-function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
-}
+const conf = useRuntimeConfig()
 
-let userId = localStorage.getItem("chatfaq-user-identifier")
-if(!userId) {
-    localStorage.setItem("chatfaq-user-identifier", uuidv4())
-    userId = localStorage.getItem("chatfaq-user-identifier")
-}
-
-const { public: { chatfaqWS, chatfaqAPI } } = useRuntimeConfig()
-console.log("PUBLIC CONFIG:::")
-console.log(chatfaqWS)
-console.log(chatfaqAPI)
-console.log(":::")
+const chatfaqWS = ref(conf.public.chatfaqWS)
+const chatfaqAPI = ref(conf.public.chatfaqAPI)
+const userId = ref(undefined)
 const title = ref("Hello there 👋")
 const subtitle = ref("How can we help you?")
 const fsmDef = ref("simple_fsm")
 
+
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+if (process.client) {
+
+    userId.value = localStorage.getItem("chatfaq-user-identifier")
+    if (!userId.value) {
+        localStorage.setItem("chatfaq-user-identifier", uuidv4())
+        userId.value = localStorage.getItem("chatfaq-user-identifier")
+    }
+}
 </script>
