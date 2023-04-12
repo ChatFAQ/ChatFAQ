fce3070b2051db506dcce3d70b2c5c17195b257f
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2023-04-12 20:13:07 +0200

    fix(widget): found the syntax for runtime env vars!

--- widget/nuxt.config.ts
+++ widget/nuxt.config.ts
@@ -19,12 +19,17 @@ export default defineNuxtConfig({
         // The private keys which are only available server-side
         // privateKey: process.env.PRIVATE_KEY,
         // Keys within public are also exposed client-side
-        public: (() => {
-            return {
-                chatfaqAPI: process.env.CHATFAQ_BACKEND_API,
-                chatfaqWS: process.env.CHATFAQ_BACKEND_WS,
-            };
-        })()
+
+        // In order to pass env variables to the client AT RUNTIME, you have to
+        // use a specially named env variable:
+        // use the prefix NUXT_ to override defaults, and _ to split capitalization:
+        //  - for private runtime variables (server only), just NUXT_VARNAME
+        //  - for public runtime variables (both), use NUXT_PUBLIC_VARNAME
+        // these 2 can be set at runtime by defining the vars NUXT_PUBLIC_CHATFAQ_API and NUXT_PUBLIC_CHATFAQ_WS
+        public: {
+            chatfaqAPI: "",
+            chatfaqWS: "",
+        }
     },
     app: {
         head: {
