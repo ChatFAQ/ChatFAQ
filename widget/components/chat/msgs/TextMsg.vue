ee076a173181c3ccd7c7a5e0460d31c3de3057bb
Hector Soria <hector.soria@with-madrid.com>
2023-07-10 19:48:29 +0200

    computing markdown responses

--- widget/components/chat/msgs/TextMsg.vue
+++ widget/components/chat/msgs/TextMsg.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>{{ props.data.payload }}</div>
+    <div class="marked-down-content" v-html="markedDown"></div>
 </template>
 
 <script setup>
 const props = defineProps(["data"]);
+import { marked } from "marked";
+
+const markedDown = computed(() => {
+    return marked(props.data.payload);
+});
+
 </script>
+<style lang="scss">
+.marked-down-content{
+    p {
+        margin: 0;
+    }
+}
+</style>
--- widget/components/chat/msgs/llm/LMMsg.vue
+++ widget/components/chat/msgs/llm/LMMsg.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-        {{ props.data.payload.model_response }}
+        <div class="marked-down-content" v-html="markedDown"></div>
         <References :references="props.data.payload.references"></References>
     </div>
 </template>
 
 <script setup>
 import References from "~/components/chat/msgs/llm/References.vue";
+import { marked } from "marked";
 
 const props = defineProps(["data"]);
 
+const markedDown = computed(() => {
+    return marked(props.data.payload.model_response);
+});
+
 </script>
+<style lang="scss">
+.marked-down-content{
+    p {
+        margin: 0;
+    }
+}
+</style>
+
