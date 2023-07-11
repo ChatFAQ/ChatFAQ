95196122034c005835c4a88992e9e71fd39e3e1e
Hector Soria <hector.soria@with-madrid.com>
2023-07-11 11:28:26 +0200

    markdown links to custom url style

--- widget/components/chat/msgs/llm/LMMsg.vue
+++ widget/components/chat/msgs/llm/LMMsg.vue
@@ -17,9 +17,19 @@ const markedDown = computed(() => {
 
 </script>
 <style lang="scss">
-.marked-down-content{
+@import "assets/styles/variables";
+
+.marked-down-content {
     p {
         margin: 0;
+
+        a {
+            color: $chatfaq-color-primary-500;
+            background: rgba(70, 48, 117, 0.1);
+            border-radius: 4px;
+            padding: 0px 6px 0px 6px;
+            text-decoration: none;
+        }
     }
 }
 </style>
