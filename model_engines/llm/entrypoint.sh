fcf4da75872e8114c05f8a5c8df8ef6d0de42d2d
Diego Peláez Paquico <diego.pelaez@with-madrid.com>
2023-09-18 19:05:07 +0200

    Added huggingface login to vllm

--- model_engines/llm/entrypoint.sh
+++ model_engines/llm/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+huggingface-cli login --token $HUGGINGFACE_KEY # login to huggingface for private model access
 /root/miniconda/envs/vllm_venv/bin/python -m vllm.entrypoints.api_server --host 0.0.0.0 --port $PORT --swap-space $SWAP_SPACE
