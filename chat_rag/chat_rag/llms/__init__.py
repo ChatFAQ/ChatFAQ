57f9c12fbef8cbd481ee99316d8016ba38a47b31
Diego Peláez Paquico <diego.pelaez@with-madrid.com>
2023-09-29 11:47:47 +0200

    Rename from llm to client

--- chat_rag/chat_rag/llms/__init__.py
+++ chat_rag/chat_rag/llms/__init__.py
@@ -1,5 +1,5 @@
 from chat_rag.llms.base_llm import BaseLLM
 from chat_rag.llms.ggml_llm import GGMLModel
 from chat_rag.llms.hf_llm import HFModel
-from chat_rag.llms.openai_llm import OpenAIModel
+from chat_rag.chat_rag.llms.openai_client import OpenAIModel
 from chat_rag.llms.vllm_client import VLLModel
