a0a40007e439d18ca62db9fbeb1a41ae3b3b21bb
Hector Soria <hector.soria@with-madrid.com>
2023-10-25 17:24:58 +0200

    adding fonts to package

--- widget/vite.config.js
+++ widget/vite.config.js
@@ -30,7 +30,9 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ["vue"],
+            external: [
+                "vue"
+            ],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
