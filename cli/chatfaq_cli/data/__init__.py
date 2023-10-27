45a15dd40403338937f8be88cecbee4a6696fa27
Diego Peláez Paquico <diego.pelaez@with-madrid.com>
2023-10-27 10:53:01 +0200

    Remove duplicated command

--- cli/chatfaq_cli/data/__init__.py
+++ cli/chatfaq_cli/data/__init__.py
@@ -1,11 +1,10 @@
 import typer
 
-from . import knowledge_items, auto_generated_titles, knowledge_bases, intents, titles
+from . import knowledge_items, knowledge_bases, intents, titles
 
 app = typer.Typer(help="Data commands")
 app.add_typer(knowledge_bases.app, name="knowledge_bases", help="Knowledge Bases commands")
 app.add_typer(knowledge_items.app, name="knowledge_items", help="Knowledge Items commands")
-app.add_typer(auto_generated_titles.app, name="auto_generated_titles", help="Auto Generated Titles commands")
 app.add_typer(intents.app, name="intents", help="Intents commands")
 app.add_typer(titles.app, name="titles", help="Autogen Titles commands")
 
