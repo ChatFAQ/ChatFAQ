e2e334d8fa229d1ed1420e3c2dbdbcfe7362e95c
Hector Soria <hector.soria@with-madrid.com>
2023-11-20 17:14:31 +0100

    [Admin - AI Config]: Bugfix, better handling of navigation through tabs

--- admin/components/ai_config/BaseAIConfig.vue
+++ admin/components/ai_config/BaseAIConfig.vue
@@ -1,67 +1,68 @@
 <template>
-    <div class="page-title">{{ $t("aiconfiguration") }}</div>
-    <el-tabs @tab-change="changeUrl" v-model="itemType" >
-        <el-tab-pane :label="$t('retriever')" name="retriever-configs">
-            <ReadWriteView :edit="id" :add="add" apiName="retriever-configs" itemName="retriever" schemaName="RetrieverConfig"
-                :cardProps="{
+  <div class="page-title">{{ $t("aiconfiguration") }}</div>
+  <el-tabs @tab-change="changeUrl" v-model="itemType">
+    <el-tab-pane :lazy="true" :label="$t('retriever')" name="retriever-configs">
+      <ReadWriteView apiName="retriever-configs" itemName="retriever" schemaName="RetrieverConfig"
+                     :cardProps="{
                     'name': $t('name'),
                     'model_name': $t('modelname'),
                     'device': $t('device'),
                 }"
-                :tableProps="{
+                     :tableProps="{
                     'name': $t('name'),
                     'model_name': $t('modelname'),
                     'device': $t('device'),
                     'updated_date': $t('updateddate'),
                 }">
-            </ReadWriteView>
-        </el-tab-pane>
-        <el-tab-pane :label="$t('prompt')" name="prompt-configs">
-            <ReadWriteView :edit="id" :add="add" apiName="prompt-configs" itemName="prompt" schemaName="PromptConfig"
-                :cardProps="{
+      </ReadWriteView>
+    </el-tab-pane>
+    <el-tab-pane :lazy="true" :label="$t('prompt')" name="prompt-configs">
+      <ReadWriteView apiName="prompt-configs" itemName="prompt" schemaName="PromptConfig"
+                     :cardProps="{
                     'name': $t('name'),
                     'n_contexts_to_use': $t('contextsnumber'),
                 }"
-                :tableProps="{
+                     :tableProps="{
                     'name': $t('name'),
                     'n_contexts_to_use': $t('contextsnumber'),
                     'updated_date': $t('updateddate'),
                 }">
-            </ReadWriteView>
-        </el-tab-pane>
-        <el-tab-pane :label="$t('generation')" name="generation-configs">
-            <ReadWriteView :edit="id" :add="add" apiName="generation-configs" itemName="generation" schemaName="GenerationConfig"
-                :cardProps="{
+      </ReadWriteView>
+    </el-tab-pane>
+    <el-tab-pane :lazy="true" :label="$t('generation')" name="generation-configs">
+      <ReadWriteView apiName="generation-configs" itemName="generation"
+                     schemaName="GenerationConfig"
+                     :cardProps="{
                     'name': $t('name'),
                     'temperature': $t('temperature'),
                     'max_new_tokens': $t('maxtokens'),
                 }"
-                :tableProps="{
+                     :tableProps="{
                     'name': $t('name'),
                     'temperature': $t('temperature'),
                     'max_new_tokens': $t('maxtokens'),
                     'updated_date': $t('updateddate'),
                 }">
-            </ReadWriteView>
-        </el-tab-pane>
-        <el-tab-pane :label="$t('llm')" name="llm-configs">
-            <ReadWriteView :edit="id" :add="add" apiName="llm-configs" itemName="LLM" schemaName="LLMConfig"
-                :cardProps="{
+      </ReadWriteView>
+    </el-tab-pane>
+    <el-tab-pane :lazy="true" :label="$t('llm')" name="llm-configs">
+      <ReadWriteView apiName="llm-configs" itemName="LLM" schemaName="LLMConfig"
+                     :cardProps="{
                     'name': $t('name'),
                     'llm_type': $t('llmtype'),
                     'llm_name': $t('llmname'),
                 }"
-                :tableProps="{
+                     :tableProps="{
                     'name': $t('name'),
                     'llm_type': $t('llmtype'),
                     'llm_name': $t('llmname'),
                     'updated_date': $t('updateddate'),
                 }">
-            </ReadWriteView>
-        </el-tab-pane>
-        <el-tab-pane :label="$t('rag')" name="rag-configs">
-            <ReadWriteView :edit="id" :add="add" apiName="rag-configs" itemName="RAG" schemaName="RAGConfig"
-                :cardProps="{
+      </ReadWriteView>
+    </el-tab-pane>
+    <el-tab-pane :lazy="true" :label="$t('rag')" name="rag-configs">
+      <ReadWriteView apiName="rag-configs" itemName="RAG" schemaName="RAGConfig"
+                     :cardProps="{
                     'name': $t('name'),
                     'knowledge_base': $t('knowledgebase'),
                     'llm_config': $t('llmconfig'),
@@ -69,7 +70,7 @@
                     'generation_config': $t('generationconfig'),
                     'retriever_config': $t('retrieverconfig'),
                 }"
-                :tableProps="{
+                     :tableProps="{
                     'name': $t('name'),
                     'knowledge_base': $t('knowledgebase'),
                     'llm_config': $t('llmconfig'),
@@ -78,32 +79,35 @@
                     'retriever_config': $t('retrieverconfig'),
                     'updated_date': $t('updateddate'),
                 }">
-            </ReadWriteView>
-        </el-tab-pane>
-    </el-tabs>
+      </ReadWriteView>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script setup>
 import ReadWriteView from "~/components/generic/ReadWriteView.vue";
-const route = useRoute()
+
+const route = useRoute();
+const router = useRouter();
 
 const itemType = ref(route.params.itemType)
-const id = ref(route.params.id)
-const add = ref(route.path.endsWith("/add/"))
 
 function changeUrl(tabName) {
-    history.pushState({}, null, `/ai_config/${tabName}/`)
+  router.push({
+    path: `/ai_config/${tabName}/`,
+  });
 }
+
 </script>
 
 <style scoped lang="scss">
 .page-title {
-    //styleName: Title/XS/Bold;
-    font-size: 18px;
-    font-weight: 700;
-    line-height: 22px;
-    letter-spacing: 0em;
-    text-align: left;
-    margin-bottom: 16px;
+  //styleName: Title/XS/Bold;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 22px;
+  letter-spacing: 0em;
+  text-align: left;
+  margin-bottom: 16px;
 }
 </style>
--- admin/components/generic/ReadWriteView.vue
+++ admin/components/generic/ReadWriteView.vue
@@ -1,53 +1,56 @@
 <template>
-    <ReadView
-        v-if="edit === undefined && !add"
-        :apiName="apiName"
-        :itemName="itemName"
-        :cardProps="cardProps"
-        :tableProps="tableProps"
-    />
-    <WriteView
-        v-else
-        :apiName="apiName"
-        :itemName="itemName"
-        :schemaName="schemaName"
-        :edit="edit"
-        :add="add"
-    />
+  <ReadView
+      v-if="edit === undefined && !add"
+      :apiName="apiName"
+      :itemName="itemName"
+      :cardProps="cardProps"
+      :tableProps="tableProps"
+  />
+  <WriteView
+      v-else
+      :apiName="apiName"
+      :itemName="itemName"
+      :schemaName="schemaName"
+      :edit="edit"
+      :add="add"
+  />
 </template>
 
 <script setup>
 import ReadView from "~/components/generic/ReadView.vue";
 import {defineProps} from 'vue';
 import WriteView from "~/components/generic/WriteView.vue";
+
+const route = useRoute()
+
+const edit = ref(route.params.id)
+const add = ref(route.path.endsWith("/add/"))
+
+watch(route, value => {
+  add.value = route.path.endsWith("/add/")
+  edit.value = route.params.id
+}, {deep: true, immediate: true})
+
 const props = defineProps({
-    apiName: {
-        type: String,
-        mandatory: true
-    },
-    itemName: {
-        type: String,
-        mandatory: true
-    },
-    schemaName: {
-        type: String,
-        mandatory: true
-    },
-    cardProps: {
-        type: Object,
-        mandatory: true
-    },
-    tableProps: {
-        type: Object,
-        mandatory: true
-    },
-    edit: {
-        type: String,
-        mandatory: false
-    },
-    add: {
-        type: Boolean,
-        mandatory: false
-    },
+  apiName: {
+    type: String,
+    mandatory: true
+  },
+  itemName: {
+    type: String,
+    mandatory: true
+  },
+  schemaName: {
+    type: String,
+    mandatory: true
+  },
+  cardProps: {
+    type: Object,
+    mandatory: true
+  },
+  tableProps: {
+    type: Object,
+    mandatory: true
+  },
 })
 </script>
