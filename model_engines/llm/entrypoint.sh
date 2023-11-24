1fcc9b55f155141494ebf86f70a9ac2d54e5edbd
Diego Pelaez <diego.pelaez@with-madrid.com>
2023-11-24 14:57:12 +0100

    Updated openai api

--- model_engines/llm/entrypoint.sh
+++ model_engines/llm/entrypoint.sh
@@ -3,4 +3,4 @@ huggingface-cli login --token $HUGGINGFACE_KEY # login to huggingface for privat
 
 echo "Model name: $MODEL"
 
-/root/miniconda/envs/vllm_venv/bin/python -m vllm.entrypoints.api_server --host $HOST --port $PORT --swap-space $SWAP_SPACE --model $MODEL --tokenizer $MODEL --tensor-parallel-size $NUM_GPUS ----gpu-memory-utilization $GPU_MEMORY_UTILIZATION
+/root/miniconda/envs/vllm_venv/bin/python -m vllm.entrypoints.api_server --host $HOST --port $PORT --swap-space $SWAP_SPACE --model $MODEL --tokenizer $MODEL --tensor-parallel-size $NUM_GPUS --gpu-memory-utilization $GPU_MEMORY_UTILIZATION
