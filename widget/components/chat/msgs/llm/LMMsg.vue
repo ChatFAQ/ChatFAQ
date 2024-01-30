8ee7d74018294d4186b15e9696689c28e9289890
Hector Soria <hector.soria@with-madrid.com>
2024-01-30 17:27:08 +0100

    [Image Processing from SDK]: Paging image references

--- widget/components/chat/msgs/llm/LMMsg.vue
+++ widget/components/chat/msgs/llm/LMMsg.vue
@@ -4,11 +4,24 @@
         <span class="reference-index" v-if="isLast" :class="{ 'dark-mode': store.darkMode }"
               v-for="refIndex in data.referenceIndexes">{{ refIndex + 1 }}</span>
     </div>
-    <div class="separator-line" :class="{ 'dark-mode': store.darkMode }" v-if="getMarkedDownImages().length"></div>
-    <div class="reference-image-wrapper" v-if="getMarkedDownImages().length">
-        <div class="reference-image" v-for="(img, index) in getMarkedDownImages()"
+    <div class="separator-line" :class="{ 'dark-mode': store.darkMode }" v-if="getMarkedDownImages.length"></div>
+    <div class="reference-image-wrapper" v-if="getMarkedDownImages.length">
+        <div class="reference-image" v-for="(img, index) in getMarkedDownImagesTotal"
              :style="{ 'background-image': 'url(' + imageUrls[img.file_name] + ')' }">
             <div class="reference-image-index target">{{ index + 1 }}</div>
+        </div>
+        <div class="reference-image-button-paging" v-if="getMarkedDownImages.length > minImgRefs"
+             @click="displayAllImgRef = !displayAllImgRef" :class="{ 'dark-mode': store.darkMode }">
+            <span v-if="displayAllImgRef">{{ $t("viewless") }}</span>
+            <svg  v-if="displayAllImgRef" width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.5 14C11.8137 14 14.5 11.3137 14.5 8C14.5 4.68629 11.8137 2 8.5 2C5.18629 2 2.5 4.68629 2.5 8C2.5 11.3137 5.18629 14 8.5 14Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M6.5 8.66602L8.5 6.66602L10.5 8.66602" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <span v-if="!displayAllImgRef">{{ $t("viewmore") }}</span>
+            <svg v-if="!displayAllImgRef" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 14C11.3137 14 14 11.3137 14 8C14 4.68629 11.3137 2 8 2C4.68629 2 2 4.68629 2 8C2 11.3137 4.68629 14 8 14Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 7.33398L8 9.33398L6 7.33398" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
         </div>
     </div>
 </template>
@@ -23,20 +36,9 @@ const props = defineProps(["data", "isLast"]);
 const hightlight_light = "#4630751a"
 const hightlight_dark = "#1A0438"
 
-function getMarkedDownImages() {
-    const images = props.data.payload.model_response.match(/!\[([^\]]+)\][ \n]*\(([^\)]+)\)/g);
-    if (images) {
-        return images.map((image) => {
-            const imageRegex = /!\[([^\]]+)\][ \n]*\(([^\)]+)\)/;
-            const imageMatch = image.match(imageRegex);
-            return {
-                alt: imageMatch[1],
-                file_name: imageMatch[2],
-            };
-        });
-    }
-    return [];
-}
+const minImgRefs = ref(2);
+const displayAllImgRef = ref(false);
+
 
 function replaceMarkedDownImagesByReferences() {
     const images = props.data.payload.model_response.match(/!\[([^\]]+)\][ \n]*\(([^\)]+)\)/g);
@@ -77,6 +79,33 @@ const markedDown = computed(() => {
 
     return res
 });
+
+const getMarkedDownImages = computed(() => {
+    return _getMarkedDownImages()
+})
+
+function _getMarkedDownImages() {
+    const images = props.data.payload.model_response.match(/!\[([^\]]+)\][ \n]*\(([^\)]+)\)/g);
+    if (images) {
+        const res = images.map((image) => {
+            const imageRegex = /!\[([^\]]+)\][ \n]*\(([^\)]+)\)/;
+            const imageMatch = image.match(imageRegex);
+            return {
+                alt: imageMatch[1],
+                file_name: imageMatch[2],
+            };
+        });
+        return [...res, ...res, ...res, ...res, ...res, ...res, ...res, ...res]
+    }
+    return [];
+}
+
+const getMarkedDownImagesTotal = computed(() => {
+    if (displayAllImgRef.value)
+        return _getMarkedDownImages()
+    return _getMarkedDownImages().slice(0, minImgRefs.value)
+
+})
 
 const imageUrls = computed(() => {
     const res = {}
@@ -184,6 +213,26 @@ const imageUrls = computed(() => {
 
     &.dark-mode {
         background-color: $chatfaq-color-separator-dark;
+    }
+}
+
+.reference-image-button-paging {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 22px;
+    padding: 2px 6px 2px 6px;
+    border-radius: 4px;
+    background: $chatfaq-color-chatMessageReference-background-light;
+    color: $chatfaq-color-chatMessageReference-text-light;
+    cursor: pointer;
+    &.dark-mode {
+        background: $chatfaq-color-chatMessageReference-background-dark;
+        color: $chatfaq-color-chatMessageReference-text-dark;
+    }
+    > span {
+        margin-right: 4px;
     }
 }
 </style>
