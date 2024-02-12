c6e4f3e882fe6f3b45b3ec46326fcdd6555a757b
Hector Soria <hector.soria@with-madrid.com>
2024-02-12 11:57:24 +0100

    [Admin - data]: Pagination styling

--- admin/components/generic/Pagination.vue
+++ admin/components/generic/Pagination.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="demo-pagination-block">
+    <div class="pagination-wrapper">
+        <div class="total"> Total: {{total}}
+        </div>
         <el-pagination
+            v-if="total > itemsStore.pageSize"
             v-model:current-page="itemsStore.currentPage"
             v-model:page-size="itemsStore.pageSize"
             :small="small"
             :disabled="disabled"
             :background="background"
-            layout="total, prev, pager, next, jumper"
+            layout="prev, pager, next"
             :total="itemsStore?.items[props.apiUrl]?.count || 0"
             @current-change="pageChange"
         />
+        <div></div>
     </div>
 
 </template>
@@ -35,14 +39,48 @@ const pageChange = async (val: number) => {
     await itemsStore.retrieveItems($axios, props.apiUrl)
     itemsStore.loading = false
 }
-</script>
+const total = computed(() => {
+    return itemsStore?.items[props.apiUrl]?.count || 0
+})
 
-<style scoped>
-.demo-pagination-block + .demo-pagination-block {
-    margin-top: 10px;
+</script>
+<style lang="scss">
+
+.el-pagination {
+    li.number, .btn-next, .btn-prev, .btn-quicknext, .btn-quickprev {
+        background-color: transparent !important;
+        border-radius: 2px !important;
+        border: 1px solid $chatfaq-color-primary-200 !important;
+        width: 32px !important;
+        height: 32px !important;
+        margin-right: 8px !important;
+        color: $chatfaq-color-greyscale-800 !important;
+        &[disabled] {
+            color: $chatfaq-color-primary-200 !important;
+            border-color: $chatfaq-color-primary-200 !important;
+        }
+        &.is-active {
+            border-color: $chatfaq-color-primary-500 !important;
+            color: $chatfaq-color-primary-500 !important;
+        }
+
+    }
 }
 
-.demo-pagination-block .demonstration {
-    margin-bottom: 16px;
+</style>
+
+<style scoped lang="scss">
+.pagination-wrapper {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    margin: 8px 16px 16px;
+
+    .total {
+        color: $chatfaq-color-greyscale-800;
+        font-size: 14px;
+        font-weight: 400;
+    }
 }
 </style>
