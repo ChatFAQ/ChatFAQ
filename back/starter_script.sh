06dbeb2d1d0b44e6437c9bbc6622e9ff5f6c6592
Hector Soria <hector.soria@with-madrid.com>
2024-04-16 17:42:11 +0200

    Verbosity

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Start the first process
-echo "Starting driver pointing to: ${REMOTE_RAY_CLUSTER_ADDRESS_HEAD}..."
+echo "Starting driver pointing to: ray:6375..."
 
 /.venv/bin/ray start --address=ray:6375 --num-cpus 0 --num-gpus 0
+
+echo "...driver started, address: ${REMOTE_RAY_CLUSTER_ADDRESS_HEAD}"
 
 # Start the second process
 modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application &
