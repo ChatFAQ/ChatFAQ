cb36eb61803e181c005c711d2be2f975d16f404e
Hector Soria <hector.soria@with-madrid.com>
2024-04-16 17:12:29 +0200

    Driver port and head port are differents

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -3,7 +3,7 @@
 # Start the first process
 echo "Starting driver pointing to: ${REMOTE_RAY_CLUSTER_ADDRESS_HEAD}..."
 
-/.venv/bin/ray start --address=ray:6379 --num-cpus 0 --num-gpus 0
+/.venv/bin/ray start --address=ray:6375 --num-cpus 0 --num-gpus 0
 
 # Start the second process
 modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application &
