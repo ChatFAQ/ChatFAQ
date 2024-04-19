5b7e1d0529fbeaf90a9040bbc8d2f26a58617c5c
Hector Soria <hector.soria@with-madrid.com>
2024-04-19 14:08:56 +0200

    Dockerfile for both: backend and celery

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -8,7 +8,14 @@ echo "Starting driver pointing to: ray:6379..."
 echo "...driver started, address: ${REMOTE_RAY_CLUSTER_ADDRESS_HEAD}"
 
 # Start the second process
-modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application &
+
+if [ -z "$IS_CELERY" ]; then
+    echo "Starting daphne..."
+    modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application &
+else
+    echo "Starting celery worker..."
+    modelw-docker run python -m celery -A back.config worker -l INFO -E --pool threads --concurrency 2 &
+fi
 
 # Wait for any process to exit
 wait -n
