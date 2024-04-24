032d33aa19574acc4663b0c519e7c1dffb22369b
Hector Soria <hector.soria@with-madrid.com>
2024-04-24 18:22:36 +0200

    New docker starter script for restarting ray driver in case of disconnection

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -7,8 +7,24 @@ echo "Starting driver pointing to: ray:6379..."
 
 echo "...driver started, address: ${REMOTE_RAY_CLUSTER_ADDRESS_HEAD}"
 
-# Start the second process
+# Function to check the health of the Ray cluster
+check_ray_health() {
+    while true; do
+        # Check the health of the Ray cluster
+        /.venv/bin/ray status --address=ray:6379 &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "Ray cluster is not healthy. Restarting the driver..."
+            /.venv/bin/ray stop
+            /.venv/bin/ray start --address=ray:6379 --num-cpus 0 --num-gpus 0
+        fi
+        sleep 5
+    done
+}
 
+# Start the health check in the background
+check_ray_health &
+
+# Start the second process
 if [ -z "$IS_CELERY" ]; then
     echo "Starting daphne..."
     modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application &
