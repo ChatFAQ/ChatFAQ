27ef32d33c9f31b9d8a4ac2eac7c5a0f4bad312e
Hector Soria <hector.soria@with-madrid.com>
2024-05-08 11:28:04 +0200

    Now the /ray/ proxy is protected with the same authentication as the rest of the API

--- admin/server/middleware/ray-proxy.js
+++ admin/server/middleware/ray-proxy.js
@@ -12,17 +12,47 @@ const rayProxyMiddleware = createProxyMiddleware('/ray/', {
     logger: console
 })
 
+function extractTokenFromRawHeaders(rawHeaders) {
+    const cookieIndex = rawHeaders.indexOf('cookie');
+    const cookieValue = rawHeaders[cookieIndex + 1];
+    if (cookieValue.indexOf(' token=') === -1)
+        return null;
+    let token = cookieValue.split(' token=')[1];
+    token = token.split(';')[0];
+    return token;
+}
+
+async function isAuthorized(event) {
+    const token = extractTokenFromRawHeaders(event.node.req.rawHeaders)
+    if (!token)
+        return false
+
+    const options = {
+        headers: {
+            'Authorization': `Token ${token}`,
+        }
+    };
+    try {
+        await $fetch(process.env.NUXT_API_URL + 'back/api/people/people/', options)
+    } catch (e) {
+        return false
+    }
+    return true
+}
+
 export default defineEventHandler(async (event) => {
+    if (event.node.req.url.startsWith('/ray/') && !await isAuthorized(event))
+        return
+
     await new Promise((resolve, reject) => {
-        const next = (err) =>
-        {
+        const next = (err) => {
             if (err) {
                 reject(err)
             } else {
                 resolve(true)
             }
         }
+        rayProxyMiddleware(event.node.req, event.node.res, next)
 
-        rayProxyMiddleware(event.req, event.res, next)
     })
 })
