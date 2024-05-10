7198c3424abb1f36fa956f0f11381dd1ebe7bb67
Hector Soria <hector.soria@with-madrid.com>
2024-05-10 12:51:22 +0200

    Making sure ray head does not run any task/job

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/.venv/bin/ray start --head --port 6375 --dashboard-host 0.0.0.0
+/.venv/bin/ray start --head --port 6375 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0
 
 modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
