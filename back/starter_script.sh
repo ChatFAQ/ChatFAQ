49df86d4fd66142ed0395ba20a64de3865b091bb
Diego Pelaez <diego.pelaez@with-madrid.com>
2024-05-14 16:38:04 +0200

    That didn't work

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -2,6 +2,4 @@
 
 /.venv/bin/ray start --head --port 6375 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0
 
-/.venv/bin/ray start --address=localhost:6375 --num-cpus=0 --num-gpus=0
-
 modelw-docker run python -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
