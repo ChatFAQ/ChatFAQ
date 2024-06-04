ada361739da2c41039ed3fdbe756efcbb476b62c
Hector Soria <hector.soria@with-madrid.com>
2024-06-04 17:21:06 +0200

    memray

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -2,5 +2,5 @@
 
 /.venv/bin/ray start --head --port 6375 --metrics-export-port=8080 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0
 
-modelw-docker run  memray run --live daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
+/.venv/bin/memray run --live daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
 # modelw-docker run daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
