cb59d0b039be79a7f9e9b3cc00c47337663dcd05
Hector Soria <hector.soria@with-madrid.com>
2024-06-04 16:44:24 +0200

    opsy, fix deploy command

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -3,4 +3,4 @@
 /.venv/bin/ray start --head --port 6375 --metrics-export-port=8080 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0
 
 # modelw-docker run memray run --live -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
-modelw-docker run -m daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
+modelw-docker run daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
