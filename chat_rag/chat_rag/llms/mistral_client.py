f3210f6ea7331baea31c99fc1e106dad3093bea4
Diego Pelaez <diego.pelaez@with-madrid.com>
2024-06-12 16:00:18 +0200

    Refactor Mistral Client

--- chat_rag/chat_rag/llms/mistral_client.py
+++ chat_rag/chat_rag/llms/mistral_client.py
@@ -11,120 +11,40 @@ from chat_rag.llms import LLM
 class MistralChatModel(LLM):
     def __init__(
         self,
-        llm_name: str,
-        base_url: str = None,
+        llm_name: str = "mistral-large-latest",
         **kwargs,
     ):
         self.client = MistralClient(api_key=os.environ["MISTRAL_API_KEY"])
+        self.aclient = MistralAsyncClient(api_key=os.environ["MISTRAL_API_KEY"])
         self.llm_name = llm_name
 
     def format_prompt(
         self,
         messages: List[Dict[str, str]],
-        contexts: List[str],
-        system_prompt: str,
-        n_contexts_to_use: int = 3,
-        lang: str = "en",
-        **kwargs,
-    ) -> List[Dict[str, str]]:
+    ) -> List[ChatMessage]:
         """
-        Formats the prompt to be used by the model.
-        Parameters
-        ----------
-        messages : List[Tuple[str, str]]
-            The messages to use for the prompt. Pair of (role, message).
-        contexts : list
-            The context to use.
-        system_prompt : str
-            The prefix to indicate instructions for the LLM.
-        n_contexts_to_use : int, optional
-            The number of contexts to use, by default 3
-        lang : str, optional
-            The language of the prompt, by default 'en'
+        Formats the prompt to be used by the model into the correct Mistral format.
         """
-        system_prompt = self.format_system_prompt(
-            contexts=contexts,
-            system_prompt=system_prompt,
-            n_contexts_to_use=n_contexts_to_use,
-            lang=lang,
-        )
-        final_messages = [ChatMessage(role='system', content=system_prompt)]  \
-            + [ChatMessage(role=message['role'], content=message['content']) for message in messages]
-        
-        print(final_messages)
+        final_messages = [
+            ChatMessage(role=message["role"], content=message["content"])
+            for message in messages
+        ]
 
         return final_messages
-    
-    def generate(
-        self,
-        messages: List[Dict[str, str]],
-        contexts: List[str],
-        prompt_structure_dict: dict,
-        generation_config_dict: dict = None,
-        lang: str = "en",
-        **kwargs,
-    ) -> str:
-        """
-        Generate text from a prompt using the model.
-        Parameters
-        ---------text-
-        messages : List[Tuple[str, str]]
-            The messages to use for the prompt. Pair of (role, message).
-        contexts : List[str]
-            The contexts to use for generation.
-        prompt_structure_dict : dict
-            Dictionary containing the structure of the prompt.
-        generation_config_dict : dict
-            Keyword arguments for the generation.
-        lang : str
-            The language of the prompt.
-        Returns
-        -------
-        str
-            The generated text.
-        """
-
-        messages = self.format_prompt(
-            messages=messages,
-            contexts=contexts,
-            **prompt_structure_dict,
-            lang=lang,
-        )
-
-        chat_response = self.client.chat(
-            model=self.llm_name,
-            messages=messages,
-            temperature=generation_config_dict["temperature"],
-            top_p=generation_config_dict["top_p"],
-            max_tokens=generation_config_dict["max_new_tokens"],
-            random_seed=generation_config_dict["seed"],
-        )
-
-        return chat_response.choices[0].message.content
 
     def stream(
         self,
         messages: List[Dict[str, str]],
-        contexts: List[str],
-        prompt_structure_dict: dict,
-        generation_config_dict: dict = None,
-        lang: str = "en",
-        **kwargs,
-    ) -> str:
+        temperature: float = 1.0,
+        max_tokens: int = 1024,
+        seed: int = None,
+    ):
         """
         Generate text from a prompt using the model in streaming mode.
         Parameters
         ----------
         messages : List[Tuple[str, str]]
             The messages to use for the prompt. Pair of (role, message).
-        contexts : List[str]
-            The contexts to use for generation.
-        prompt_structure_dict : dict
-            Dictionary containing the structure of the prompt.
-        generation_config_dict : dict
-            Keyword arguments for the generation.
-        lang : str
-            The language of the prompt.
         Returns
         -------
         str
@@ -133,95 +53,33 @@ class MistralChatModel(LLM):
 
         messages = self.format_prompt(
             messages=messages,
-            contexts=contexts,
-            **prompt_structure_dict,
-            lang=lang,
         )
 
         for chunk in self.client.chat_stream(
             model=self.llm_name,
             messages=messages,
-            temperature=generation_config_dict["temperature"],
-            top_p=generation_config_dict["top_p"],
-            max_tokens=generation_config_dict["max_new_tokens"],
-            random_seed=generation_config_dict["seed"],
+            temperature=temperature,
+            max_tokens=max_tokens,
+            random_seed=seed,
         ):
             if chunk.choices[0].delta.content is not None:
                 yield chunk.choices[0].delta.content
 
         return
 
-
-class AsyncMistralChatModel(LLM):
-    def __init__(
-        self,
-        llm_name: str,
-        **kwargs,
-    ):
-        self.client = MistralAsyncClient(api_key=os.environ["MISTRAL_API_KEY"])
-        self.llm_name = llm_name
-
-    def format_prompt(
+    async def astream(
         self,
         messages: List[Dict[str, str]],
-        contexts: List[str],
-        system_prompt: str,
-        n_contexts_to_use: int = 3,
-        lang: str = "en",
-        **kwargs,
-    ) -> List[Dict[str, str]]:
+        temperature: float = 1.0,
+        max_tokens: int = 1024,
+        seed: int = None,
+    ):
         """
-        Formats the prompt to be used by the model.
+        Generate text from a prompt using the model in streaming mode.
         Parameters
         ----------
         messages : List[Tuple[str, str]]
             The messages to use for the prompt. Pair of (role, message).
-        contexts : list
-            The context to use.
-        system_prompt : str
-            The prefix to indicate instructions for the LLM.
-        n_contexts_to_use : int, optional
-            The number of contexts to use, by default 3
-        lang : str, optional
-            The language of the prompt, by default 'en'
-        """
-        system_prompt = self.format_system_prompt(
-            contexts=contexts,
-            system_prompt=system_prompt,
-            n_contexts_to_use=n_contexts_to_use,
-            lang=lang,
-        )
-        final_messages = [ChatMessage(role='system', content=system_prompt)]  \
-            + [ChatMessage(role=message['role'], content=message['content']) for message in messages]
-        
-        print(final_messages)
-
-        return final_messages
-  
-
-    async def generate(
-        self,
-        messages: List[Dict[str, str]],
-        contexts: List[str],
-        prompt_structure_dict: dict,
-        generation_config_dict: dict = None,
-        lang: str = "en",
-        **kwargs,
-    ) -> str:
-        """
-        Generate text from a prompt using the model.
-        Parameters
-        ---------
-        messages : List[Tuple[str, str]]
-            The messages to use for the prompt. Pair of (role, message).
-        contexts : List[str]
-            The contexts to use for generation.
-        prompt_structure_dict : dict
-            Dictionary containing the structure of the prompt.
-        generation_config_dict : dict
-            Keyword arguments for the generation.
-        lang : str
-            The language of the prompt.
         Returns
         -------
         str
@@ -230,67 +88,82 @@ class AsyncMistralChatModel(LLM):
 
         messages = self.format_prompt(
             messages=messages,
-            contexts=contexts,
-            **prompt_structure_dict,
-            lang=lang,
         )
 
-        chat_response = await self.client.chat(
+        async for chunk in self.aclient.chat_stream(
             model=self.llm_name,
             messages=messages,
-            temperature=generation_config_dict["temperature"],
-            top_p=generation_config_dict["top_p"],
-            max_tokens=generation_config_dict["max_new_tokens"],
-            random_seed=generation_config_dict["seed"],
+            temperature=temperature,
+            max_tokens=max_tokens,
+            random_seed=seed,
+        ):
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
+
+        return
+
+    def generate(
+        self,
+        messages: List[Dict[str, str]],
+        temperature: float = 1.0,
+        max_tokens: int = 1024,
+        seed: int = None,
+    ):
+        """
+        Generate text from a prompt using a model.
+        Parameters
+        ----------
+        messages : List[Tuple[str, str]]
+            The messages to use for the prompt. Pair of (role, message).
+        Returns
+        -------
+        str
+            The generated text.
+        """
+
+        messages = self.format_prompt(
+            messages=messages,
+        )
+
+        chat_response = self.client.chat(
+            model=self.llm_name,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            random_seed=seed,
         )
 
         return chat_response.choices[0].message.content
 
-    async def stream(
+    async def agenerate(
         self,
         messages: List[Dict[str, str]],
-        contexts: List[str],
-        prompt_structure_dict: dict,
-        generation_config_dict: dict = None,
-        lang: str = "en",
-        **kwargs,
-    ) -> str:
-            """
-            Generate text from a prompt using the model in streaming mode.
-            Parameters
-            ---------
-            messages : List[Tuple[str, str]]
-                The messages to use for the prompt. Pair of (role, message).
-            contexts : List[str]
-                The contexts to use for generation.
-            prompt_structure_dict : dict
-                Dictionary containing the structure of the prompt.
-            generation_config_dict : dict
-                Keyword arguments for the generation.
-            lang : str
-                The language of the prompt.
-            Returns
-            -------
-            str
-                The generated text.
-            """
-            
-            messages = self.format_prompt(
-                messages=messages,
-                contexts=contexts,
-                **prompt_structure_dict,
-                lang=lang,
-            )
-    
-            async for chunk in self.client.chat_stream(
-                model=self.llm_name,
-                messages=messages,
-                temperature=generation_config_dict["temperature"],
-                top_p=generation_config_dict["top_p"],
-                max_tokens=generation_config_dict["max_new_tokens"],
-                random_seed=generation_config_dict["seed"],
-            ):
-                if chunk.choices[0].delta.content is not None:
-                    yield chunk.choices[0].delta.content
-    
-            return
+        temperature: float = 1.0,
+        max_tokens: int = 1024,
+        seed: int = None,
+    ):
+        """
+        Generate text from a prompt using a model.
+        Parameters
+        ----------
+        messages : List[Tuple[str, str]]
+            The messages to use for the prompt. Pair of (role, message).
+        Returns
+        -------
+        str
+            The generated text.
+        """
+
+        messages = self.format_prompt(
+            messages=messages,
+        )
+
+        chat_response = await self.aclient.chat(
+            model=self.llm_name,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            random_seed=seed,
+        )
+
+        return chat_response.choices[0].message.content
