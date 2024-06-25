e72c9bd0a080eb8a30672ca5c2ad521db9de60a9
Diego Pelaez <diego.pelaez@with-madrid.com>
2024-06-25 16:43:19 +0200

    Trying to fix Twisted

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -5,4 +5,4 @@ export RAY_task_events_max_num_task_in_gcs=100
 /.venv/bin/ray start --head --port 6375 --metrics-export-port=8080 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0
 
 # /.venv/bin/memray run -f --output mem.bin /.venv/bin/daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
-modelw-docker run daphne -v 3 -b 0.0.0.0 -p 8000 back.config.asgi:application
+modelw-docker run daphne -v 1 -b 0.0.0.0 -p 8000 back.config.asgi:application
