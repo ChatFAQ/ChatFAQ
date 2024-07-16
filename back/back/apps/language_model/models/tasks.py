c66bd68ff533fb0819179fb8f15844ad90daf5ee
Hector Soria <hector.soria@with-madrid.com>
2024-07-16 15:05:05 +0200

    fix

--- back/back/apps/language_model/models/tasks.py
+++ back/back/apps/language_model/models/tasks.py
@@ -58,7 +58,7 @@ class RayTaskState(models.Model):
 
         parse_task_states_data = RayTaskStateSerializer(parse_task_states, many=True).data
 
-        ray_task_states = ray_api.list_tasks()
+        ray_task_states = ray_api.list_tasks(raise_on_missing_output=False)
         tasks = []
         for task in ray_task_states:
             tasks.append(ray_api.get_task(task.task_id))
