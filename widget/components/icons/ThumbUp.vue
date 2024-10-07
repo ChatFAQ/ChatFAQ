7a1df9ef2cefc91c5fa3e9f24c0b9c779c35bf57
Hector Soria <hector.soria@with-madrid.com>
2024-10-07 14:05:39 +0200

    Configurable conlor for user feedback controls

--- widget/components/icons/ThumbUp.vue
+++ widget/components/icons/ThumbUp.vue
@@ -1,9 +1,9 @@
 <template>
 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g opacity="0.4">
-<path d="M2 7C2 6.44772 2.44772 6 3 6H4.66667V14H3C2.44772 14 2 13.5523 2 13V7Z" stroke="#463075" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M4.66602 7.33333V12.6667L5.65832 13.3282C6.31539 13.7662 7.08742 14 7.87712 14H10.1246C11.5912 14 12.8427 12.9398 13.0838 11.4932L13.6112 8.3288C13.8144 7.10973 12.8743 6 11.6384 6H9.33268" stroke="#463075" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9.33268 6L9.7908 3.70944C9.90994 3.11371 9.56684 2.5225 8.9905 2.33038V2.33038C8.42178 2.14081 7.80021 2.39827 7.53212 2.93447L5.33268 7.33333H4.66602" stroke="#463075" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2 7C2 6.44772 2.44772 6 3 6H4.66667V14H3C2.44772 14 2 13.5523 2 13V7Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4.66602 7.33333V12.6667L5.65832 13.3282C6.31539 13.7662 7.08742 14 7.87712 14H10.1246C11.5912 14 12.8427 12.9398 13.0838 11.4932L13.6112 8.3288C13.8144 7.10973 12.8743 6 11.6384 6H9.33268" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9.33268 6L9.7908 3.70944C9.90994 3.11371 9.56684 2.5225 8.9905 2.33038V2.33038C8.42178 2.14081 7.80021 2.39827 7.53212 2.93447L5.33268 7.33333H4.66602" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </g>
 </svg>
 </template>
