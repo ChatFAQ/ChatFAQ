0bb7bd5020eef7f7daca8eecf4e9b51d0763860e
Diego Pelaez <diego.pelaez@with-madrid.com>
2024-11-27 15:46:37 +0100

    Update dependencies and don't run ray head

--- back/starter_script.sh
+++ back/starter_script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export RAY_task_events_max_num_task_in_gcs=100
+# Check if USE_RAY is set to "True"
+if [ "$USE_RAY" = "True" ]; then
+    export RAY_task_events_max_num_task_in_gcs=100
 
-/.venv/bin/ray start --head --port 6375 --metrics-export-port=8080 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0 --object-store-memory 500000000
+    /.venv/bin/ray start --head --port 6375 --metrics-export-port=8080 --num-cpus 0 --num-gpus 0 --dashboard-host 0.0.0.0 --object-store-memory 500000000
+fi
 
 # /.venv/bin/memray run -f --output mem.bin /.venv/bin/daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
 # modelw-docker run daphne -b 0.0.0.0 -p 8000 back.config.asgi:application
