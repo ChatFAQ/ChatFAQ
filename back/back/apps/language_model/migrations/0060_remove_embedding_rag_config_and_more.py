48532f6345be0f379328327d958ab725b93baf32
rando128 <sthonore@Stephanes-MacBook-Pro-3.local>
2024-12-17 18:24:32 +0100

    Try and fix migration 0060

--- back/back/apps/language_model/migrations/0060_remove_embedding_rag_config_and_more.py
+++ back/back/apps/language_model/migrations/0060_remove_embedding_rag_config_and_more.py
@@ -49,6 +49,7 @@ class Migration(migrations.Migration):
             model_name="retrieverconfig",
             name="knowledge_base",
             field=models.ForeignKey(
+                null=True,
                 on_delete=django.db.models.deletion.CASCADE,
                 to="language_model.knowledgebase",
             ),
