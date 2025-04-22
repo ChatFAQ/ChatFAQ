1765b5da3444b9d1840ed748862eb999d3d1ce96
Diego Pelaez <diego.pelaez@with-madrid.com>
2025-04-22 17:20:32 +0200

    Don't run the module sim for now

--- back/back/apps/health/resolver.py
+++ back/back/apps/health/resolver.py
@@ -276,29 +276,29 @@ def build_resolver() -> Resolver:
         )
     )
 
-    resolver.register(
-        Instance(
-            code="M001",
-            check=Module1Simulation(),
-            depends_on=[],
-        )
-    )
+    # resolver.register(
+    #     Instance(
+    #         code="M001",
+    #         check=Module1Simulation(),
+    #         depends_on=[],
+    #     )
+    # )
 
-    resolver.register(
-        Instance(
-            code="M002",
-            check=Module2Simulation(),
-            depends_on=[],
-        )
-    )
+    # resolver.register(
+    #     Instance(
+    #         code="M002",
+    #         check=Module2Simulation(),
+    #         depends_on=[],
+    #     )
+    # )
 
-    resolver.register(
-        Instance(
-            code="M003",
-            check=Module3Simulation(),
-            depends_on=[],
-        )
-    )
+    # resolver.register(
+    #     Instance(
+    #         code="M003",
+    #         check=Module3Simulation(),
+    #         depends_on=[],
+    #     )
+    # )
 
     resolver.register(
         Instance(
