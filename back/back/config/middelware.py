6372731e9129731fcc0edcf3adcc38c80baa1708
Hector Soria <hector.soria@with-madrid.com>
2025-06-16 16:07:24 +0200

    Async view for querying the LLM

--- back/back/config/middelware.py
+++ back/back/config/middelware.py
@@ -12,6 +12,11 @@ from rest_framework.permissions import BasePermission
 from back.apps.widget.models import Widget
 from urllib.parse import urlparse
 
+from functools import wraps
+from asgiref.sync import sync_to_async
+from knox.auth import TokenAuthentication
+from django.http import JsonResponse
+
 logger = getLogger(__name__)
 
 
@@ -90,3 +95,24 @@ class IsAuthenticatedOrWidgetOriginHostPermission(BasePermission):
         if fnmatch.fnmatch(urlparse(origin).netloc, widget.domain):
             return True
         return False
+
+class KnoxAsyncAuthMixin:
+    """Mixin to add Knox authentication to async class-based views."""
+
+    async def dispatch(self, request, *args, **kwargs):
+        knox_auth = TokenAuthentication()
+
+        async def authenticate_async():
+            try:
+                return await sync_to_async(knox_auth.authenticate)(request)
+            except Exception as e:
+                return None
+
+        auth_result = await authenticate_async()
+
+        if auth_result:
+            request.user = auth_result[0]
+            request.auth = auth_result[1]
+            return await super().dispatch(request, *args, **kwargs)
+        else:
+            return JsonResponse({"error": "Authentication failed"}, status=401)
